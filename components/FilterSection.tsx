8d17f34b11fa2c181e61292c02cc6c7168f80ef3
waterim <waterim3009@gmail.com>
2022-12-08 12:36:13 +0100

    mobile optimization

--- components/FilterSection.tsx
+++ components/FilterSection.tsx
@@ -10,7 +10,7 @@ const FilterSection = ({ filterAction, searchAction }: FilterSectionProps) => {
   const router = useRouter();
   const currentQuery = router.query;
   return (
-    <div className="bg-white px-8 py-4 flex items-center drop-shadow-md rounded-lg">
+    <div className="bg-white px-8 py-4 flex items-center drop-shadow-md rounded-lg max-[500px]:flex-col">
       <input
         className="text-xl px-4 py-2 outline-none border-b-[0.8px] border-yellow-400"
         placeholder="Search..."
